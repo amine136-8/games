47598815850c0adfb9932dfb38bd04856db4452a
amine136-8 <sidlakhsar@gmail.com>
2026-05-15 22:44:59 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,89 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-room",
+      "name": "Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5,
+          "pitch": -0.15,
+          "rotation": 0,
+          "target": "1-sallon",
+          "title": "Go to Sallon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sallon",
+      "name": "Sallon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.641592653589793,
+          "pitch": -0.15,
+          "rotation": 3.141592653589793,
+          "target": "0-room",
+          "title": "Go to Room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
